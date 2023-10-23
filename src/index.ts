8021df2366842dcf6650301d830f69d47eb18a56
DelgadoElias <eliaslautarodelgado@gmail.com>
2023-10-23 16:30:39 -0300

    commit

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,10 @@ const app = express();
 const port = process.env.PORT || 3333;
 
 app.use(express.json());
-app.use(express.raw({ type: "application/vnd.custom-type" }));
-app.use(express.text({ type: "text/html" }));
-
+/** 
+   app.use(express.raw({ type: "application/vnd.custom-type" }));
+   app.use(express.text({ type: "text/html" }));
+*/
 app.get("/", async (req, res) => {
   res.json({ message: "Please visit /countries to view all the countries" });
 });
